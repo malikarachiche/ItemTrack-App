2a86d49def46b0792f06c9cf84fe21cfb0fe0723
Malik Arachiche <arachich@msu.edu>
2018-07-24 09:36:37 -0700

    Initial Commit

---
+++ Forget/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Forget
+//
+//  Created by Malik Arachiche on 7/24/18.
+//  Copyright © 2018 Malik Arachiche. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
