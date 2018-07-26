6a6daa1873ce1da455cea11c500e18148701fee0
Malik Arachiche <arachich@msu.edu>
2018-07-26 10:53:41 -0700

    Rough UI Finished, working on view controllers

---
+++ Forget/CustomListViewController.swift
@@ -0,0 +1,39 @@
+//
+//  CustomListViewController.swift
+//  Forget
+//
+//  Created by Malik Arachiche on 7/26/18.
+//  Copyright © 2018 Malik Arachiche. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CustomListViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    @IBAction func setTimeButtonAction(_ sender: UIButton) {
+    }
+    
+
+    @IBAction func textFieldEditingBegin(_ sender: UITextField) {
+    }
+    
+    @IBAction func textFieldEditingEnd(_ sender: UITextField) {
+    }
+    
+    @IBAction func textFieldCancel(_ sender: UITextField) {
+    }
+    
+}
+
+
+
--- Forget/ViewController.swift
+++ Forget/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var homeScreenLabel: UILabel!
+    @IBOutlet weak var preMadebutton: UIButton!
+    @IBOutlet weak var customButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func preMadeButtonAction(_ sender: UIButton) {
+    }
+    @IBAction func customButtonAction(_ sender: UIButton) {
+    }
+    
 }
-
